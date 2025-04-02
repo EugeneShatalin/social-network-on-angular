d08bc03ab35a5fcb4db1893f8becae197c8f4e16
EugeneShatalin <shatalin_ei@mail.ru>
2025-04-02 20:38:55 +0500

    закончил теоретический материал по бесконечному скроллу

--- libs/profile/src/lib/data/store/effects.ts
+++ libs/profile/src/lib/data/store/effects.ts
@@ -1,8 +1,10 @@
 import {inject, Injectable} from '@angular/core';
-import { ProfileService } from '../services/profile.service';
+import {ProfileService} from '../services/profile.service';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import {profileActions} from './actions';
-import {map, switchMap} from 'rxjs';
+import {map, switchMap, withLatestFrom} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {selectFilteredProfiles, selectProfileFilters, selectProfilePageable} from './selectors';
 
 
 @Injectable({
@@ -11,14 +13,25 @@ import {map, switchMap} from 'rxjs';
 export class ProfileEffects {
   profileService = inject(ProfileService);
   actions$ = inject(Actions)
+  store = inject(Store)
 
   filterProfiles = createEffect(() => {
     return this.actions$.pipe(
       //ofType отслеживает каккой экшен произошел и пропускает выполнение кода дальше
       // если указанный в его параметраш экшен совпадает с произошедшим
-      ofType(profileActions.filterEvents),
-      switchMap(({filters}) => {
-        return this.profileService.filterProfiles(filters)
+      ofType(
+        profileActions.filterEvents,
+        profileActions.setPage
+      ),
+      withLatestFrom(
+        this.store.select(selectProfileFilters),
+        this.store.select(selectProfilePageable),
+      ),
+      switchMap(([_, filters, pageable]) => {
+        return this.profileService.filterProfiles({
+          ...pageable,
+          ...filters
+        })
       }),
       map(res => profileActions.profilesLoaded({profiles: res.items}))
     )
--- libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
+++ libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
@@ -1,20 +1,28 @@
-import { Component, inject } from '@angular/core';
-import { ProfileCardComponent } from '../../ui/profile-card/profile-card.component';
-import { ProfileFiltersComponent } from '../profile-filters/profile-filters.component';
-import { AsyncPipe } from '@angular/common';
-import {selectFilteredProfiles} from '@tt/profile';
+import {Component, inject} from '@angular/core';
+import {ProfileCardComponent} from '../../ui/profile-card/profile-card.component';
+import {ProfileFiltersComponent} from '../profile-filters/profile-filters.component';
+import {AsyncPipe} from '@angular/common';
+import {profileActions, selectFilteredProfiles} from '@tt/profile';
 import {Store} from '@ngrx/store';
+import {InfiniteScrollTriggerComponent} from '@tt/common-ui';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [ProfileCardComponent, ProfileFiltersComponent, AsyncPipe],
+  imports: [ProfileCardComponent, ProfileFiltersComponent, AsyncPipe, InfiniteScrollTriggerComponent],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
 })
 export class SearchPageComponent {
   store = inject(Store)
   profiles = this.store.selectSignal(selectFilteredProfiles)
+  //чтоб консоль была доступна в HTML шаблоне
+  console = console
 
-  constructor() {}
+  constructor() {
+  }
+
+  timeToFetch() {
+    this.store.dispatch(profileActions.setPage({}))
+  }
 }
